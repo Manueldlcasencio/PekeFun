0ded01a316bbb212dafa5d2ce9210a5d22c619ed
angemonteverde <angemonteverde@gmail.com>
2023-03-27 14:42:20 +0000

    features: navbar, carrusel en proceso

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -15,6 +15,8 @@ export const Home = () => {
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
+
+			
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
